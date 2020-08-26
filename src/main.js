c51ce9255f1c271cf56733b53264ddda54d70fc8
Your Name <1121853541@qq.com>
2020-08-26 19:41:11 +0800

    配置项初始化

--- src/main.js
+++ src/main.js
@@ -1,6 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import './styles/base.less'
+import './styles/iconfont.css'
+import Vant from 'vant'
+import 'vant/lib/index.css'
+
+Vue.use(Vant)
 
 Vue.config.productionTip = false
 
---
+++ src/views/Login.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    我是登陆页面
+    <span class="iconfont iconwode"></span>
+    <span class="iconfont icondianzan"></span>
+    <van-button loading type="primary" />
+    <van-button loading type="primary" loading-type="spinner" />
+    <van-button loading type="info" loading-text="加载中..." />
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style>
+</style>
